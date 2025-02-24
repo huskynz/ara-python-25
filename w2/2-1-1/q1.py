cd56e3bc02d031ae3552d5e32ec067c79a334cbf
Peter Gallwas <peter@husky.nz>
2025-02-25 11:12:41 +1300

    Add 2-1-1 q1 to q3 - q3 AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

---
+++ w2/2-1-1/q1.py
@@ -0,0 +1,12 @@
+# 1-1-1
+# Peter
+# peter@husky.nz
+# wow
+
+ufname=input("Please enter your first name: ")
+
+ufnamelower = ufname.lower()
+ufnameupper = ufname.upper()
+ufnameupcase = ufname.capitalize()
+
+print(f"{ufnameupcase}, your first name in lower case is {ufnamelower} and in upper case is {ufnameupper}.")
---
+++ w2/2-1-1/q2.py
@@ -0,0 +1,20 @@
+# 1-1-1
+# Peter
+# peter@husky.nz
+# wow
+
+
+ufname=input("Please enter your first name: ")
+
+ulname=input("Please enter your last name: ")
+
+ccode="BCDE101"
+
+ufnamestrip= ufname.strip()
+ulnamestrip= ulname.strip()
+
+ufnamecap=ufnamestrip.capitalize()
+
+ulnamecap=ulnamestrip.capitalize() 
+
+print(f"Pleased to meet you {ufnamecap} {ulnamecap}, welcome to {ccode}.")
