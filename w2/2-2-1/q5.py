d4aea7d08fe95185f78299a59ca4c50430430eb3
Peter Gallwas <peter@husky.nz>
2025-03-06 11:14:29 +1300

    Add q4 and q5 start

---
+++ w2/2-2-1/q5.py
@@ -0,0 +1,10 @@
+# 1-1-1
+# Peter
+# peter@husky.nz
+# wow
+
+beachtoday = input("Should we go to the beach? (y/n): ")
+issaturday = input("Is it Saturday? (y/n): ")
+issunday = input("Is it Sunday? (y/n): ")
+istideout = input("Is the tide out? (y/n): ")
+
