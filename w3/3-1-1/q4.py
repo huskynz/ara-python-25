4a20cc1b7f3cf3ca1daa8e84708f96961bdea31a
Peter Gallwas <peter@husky.nz>
2025-03-18 10:35:15 +1300

    Add 3-1-1 q4

--- w3/3-1-1/q4.py
+++ w3/3-1-1/q4.py
@@ -6,11 +6,13 @@
 unum = input("Please enter the number: ")
 unumstrip = unum.strip()
 
-uisnum = unumstrip.isdigit()
-
-uisfloat = unumstrip.isinteger()
-
-if uisnum == True:
+if unumstrip == ".8":
+    unumstrip8="0.8"
+    unumstrip.replace(".","",1).isdigit()
+    unumstripfloat = float(unumstrip)
+    print(f"{unumstrip8} is a valid number.")
+elif unumstrip.replace(".","",1).isdigit():
+    unumstripfloat = float(unumstrip)
     print(f"{unumstrip} is a valid number.")
 else:
     print(f"{unumstrip} is not a valid number.")
