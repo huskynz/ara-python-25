2e728f496e1f4e7152751d258d20914ce61d7002
Peter Gallwas <peter@husky.nz>
2025-04-01 11:32:32 +1300

    w6 q2 and q3

---
+++ w6/6-1-1/q2.py
@@ -0,0 +1,11 @@
+# 1-1-1
+# Peter
+# peter@husky.nz
+# wow
+
+def get_option(prompt_msg="Option: "):
+    """
+    THis works somehow, my code works somewhat
+    """
+    umess = input(f"{prompt_msg}")
+    return umess.lower().strip()
